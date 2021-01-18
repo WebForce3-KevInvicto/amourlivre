34c5f30c7557349bbfb269fd40430e097e58635e
KevInvicto <kevinvicto139@gmail.com>
2021-01-18 10:40:03 +0100

    ajout routes pour pages du footer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -52,4 +52,59 @@ class MainController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        
+        return $this->render('footer/contact.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="cgu")
+     */
+    public function cgu(): Response
+    {
+        
+        return $this->render('footer/cgu.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/cgv", name="cgv")
+     */
+    public function cgv(): Response
+    {
+        
+        return $this->render('footer/cgv.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        
+        return $this->render('footer/about.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/mentions", name="mentions")
+     */
+    public function mentions(): Response
+    {
+        
+        return $this->render('footer/mentions.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
 }
