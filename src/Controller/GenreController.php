11b4e2b158d9e93f7497e44b20ff2a6fad1ca76a
CelBet <celinebetoulieres@gmail.com>
2021-01-26 17:17:16 +0100

    css sur admin /wip

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/genre")
+ * @Route("/admin/genre")
  */
 class GenreController extends AbstractController
 {
